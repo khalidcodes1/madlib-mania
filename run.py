fa8b9e8b8f993079b37dbb630541a826a5b16bc9
khalid sodal <sodalkhalid10@gmail.com>
2023-11-08 16:29:25 +0000

    Add the rules of the game

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
+def show_the_rules():
+    """
+    Shows the rules of the game
+    """
+    print(f'''
+    1.Follow the instructions!
+    2.Type the words carefully!
+    ''')
 
+
+show_the_rules()
