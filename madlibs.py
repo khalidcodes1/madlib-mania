1bf0deee2e06338bc125f23c405303626d3f04f6
khalid sodal <sodalkhalid10@gmail.com>
2023-11-11 20:24:57 +0000

    Add corrected validation method for second story inputs

--- madlibs.py
+++ madlibs.py
@@ -27,7 +27,7 @@ def madlib():
         else:
             get_validated_input(100, 2, descriptive_word)
     verb = ""
-    valid_verb = False 
+    valid_verb = False
     while valid_verb is False:
         verb = input('Please enter a verb:')
         if get_validated_input(100, 2, verb):
@@ -92,8 +92,7 @@ def madlib():
         if get_validated_input(100, 2, measure_of_time):
             valid_measure_of_time = True
         else:
-            get_validated_input(100, 2, measure_of_time)
-            
+            get_validated_input(100, 2, measure_of_time)    
     color2 = ""
     valid_color2 = False 
     while valid_color2 is False:
@@ -112,7 +111,7 @@ def madlib():
             get_validated_input(100, 2, animal2)
     number2 = validate_number()
     silly_word = ""
-    valid_silly_word = False 
+    valid_silly_word = False
     while valid_silly_word is False:
         silly_word = input('Please enter a silly word:')
         if get_validated_input(100, 2, silly_word):
@@ -144,51 +143,149 @@ def madlib():
 def madlib_2():
     """ Asks the user for input, validates the input and returns the string"""
     # User input variables
-    number = ""
+    number = validate_number()
     measure_of_time = ""
+    valid_measure_of_time = False 
+    while valid_measure_of_time is False:
+        measure_of_time = input('Please enter a measure of time:')
+        if get_validated_input(100, 2, measure_of_time):
+            valid_measure_of_time = True
+        else:
+            get_validated_input(100, 2, measure_of_time)
     transportation_mode = ""
+    valid_transportation_mode = False 
+    while valid_transportation_mode is False:
+        transportation_mode = input('Please enter a transportation mode:')
+        if get_validated_input(100, 2, transportation_mode):
+            valid_measure_of_time = True
+        else:
+            get_validated_input(100, 2, transportation_mode)
     descriptive_word = ""
+    valid_descriptive_word = False 
+    while valid_descriptive_word is False:
+            descriptive_word = input('Please enter an adjective:')
+        if get_validated_input(100, 2, descriptive_word):
+            valid_descriptive_word = True
+        else:
+            get_validated_input(100, 2, descriptive_word)
     descriptive_word2 = ""
+    valid_descriptive_word2 = False
+    while valid_descriptive_word2 is False:
+            descriptive_word2 = input('Please enter an adjective:')
+        if get_validated_input(100, 2, descriptive_word2):
+            valid_descriptive_word2 = True
+        else:
+            get_validated_input(100, 2, descriptive_word2)
     object1 = ""
+    valid_object1 = False
+    while valid_object1 is False:
+        object1 = input('Please enter a noun:')
+        if get_validated_input(100, 2, object1):
+            valid_object1 = True
+        else:
+            get_validated_input(100, 2, object1)
     color = ""
+    valid_color = False 
+    while valid_color is False:
+        color = input('Please enter a color:')
+        if get_validated_input(100, 2, color):
+            valid_color = True
+        else:
+            get_validated_input(100, 2, color)
     part_of_body = ""
+    valid_part_of_body = False
+    while valid_part_of_body is False:
+        part_of_body = input('Please enter a body part:')
+        if get_validated_input(100, 2, part_of_body):
+            valid_part_of_body = True
+        else:
+            get_validated_input(100, 2, part_of_body)
     verb = ""
-    number2 = ""
+    valid_verb = False
+    while valid_verb is False:
+        verb = input('Please enter a verb:')
+        if get_validated_input(100, 2, verb):
+            valid_verb = True
+        else:
+            get_validated_input(100, 2, verb)
+    number2 = validate_number()
     object2 = ""
+    valid_object2 = False
+    while valid_object2 is False:
+        object2 = input('Please enter a noun:')
+        if get_validated_input(100, 2, object2):
+            valid_object2 = True
+        else:
+            get_validated_input(100, 2, object2)
     object3 = ""
+    valid_object3 = False
+    while valid_object3 is False:
+        object3 = input('Please enter a noun:')
+        if get_validated_input(100, 2, object3):
+            valid_object3 = True
+        else:
+            get_validated_input(100, 2, object3)
     part_of_body2 = ""
+    valid_part_of_body2 = False 
+    while valid_part_of_body2 is False:
+        part_of_body2 = input('Please enter a body part:')
+        if get_validated_input(100, 2, part_of_body2):
+            valid_part_of_body2 = True
+        else:
+            get_validated_input(100, 2, part_of_body2)
     verb2 = ""
+    valid_verb2 = False 
+    while valid_verb2 is False:
+        verb2 = input('Please enter a verb:')
+        if get_validated_input(100, 2, verb2):
+            valid_verb2 = True
+        else:
+            get_validated_input(100, 2, verb2)
     object4 = ""
+    valid_object4 = False
+    while valid_object4 is False:
+        object4 = input('Please enter a noun:')
+        if get_validated_input(100, 2, object4):
+            valid_object4 = True
+        else:
+            get_validated_input(100, 2, object4)
     descriptive_word3 = ""
+    valid_descriptive_word3 = False
+    while valid_descriptive_word3 is False:
+            descriptive_word3 = input('Please enter an adjective:')
+        if get_validated_input(100, 2, descriptive_word3):
+            valid_descriptive_word3 = True
+        else:
+            get_validated_input(100, 2, descriptive_word3)
     silly_word = ""
+    valid_silly_word = False 
+    while valid_silly_word is False:
+        silly_word = input('Please enter a silly word:')
+        if get_validated_input(100, 2, silly_word):
+            valid_silly_word = True
+        else:
+            get_validated_input(100, 2, silly_word)
     object5 = ""
-
-    # Checking user input
-    number = ValidateNumber()
-    measure_of_time = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a measure of time:')   
-    transportation_mode = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a mode of transport:') 
-    descriptive_word = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter an adjective:')       
-    descriptive_word2 = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter an adjective:')  
-    object1 = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a noun:')
-    color = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a color:')  
-    part_of_body = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a body part:')     
-    verb = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a verb:')
-    number2 = ValidateNumber()
-    object2 = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a noun:')    
-    object3 = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a noun:')    
-    part_of_body2 = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a body part:')
-    verb2 = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a verb:')
-    object4 = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a noun:')    
-    descriptive_word3 = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter an adjective:')     
-    silly_word = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a silly word:')
-    object5 = get_validated_input(maxStringLength=12, minStringLength=3, inputPrompt='Please enter a noun:')
+    valid_object5 = False
+    while valid_object5 is False:
+        object5 = input('Please enter a noun:')
+        if get_validated_input(100, 2, object5):
+            valid_object5 = True
+        else:
+            get_validated_input(100, 2, object5)
  
-
-    second_madlib = f'''It was about {number} {measure_of_time} ago when I came to the hospital in a {transportation_mode}. The hospital is a {descriptive_word}
-    place, there are a lot of {descriptive_word2} {object1} here. There are nurses here who have {color} {part_of_body}. If someone wants to come
-    into my room I told them that they have to {verb} first.I have decorated my room with {number2} {object2}. Today a 
-    doctor came into my room and they were wearing a {object3} on their {part_of_body2}. I heard that all doctors {verb2} {object4} 
-    everyday for breakfast. The most {descriptive_word3} thing about being in the hospital is the {silly_word} {object5}.'''
+    second_madlib = f'''
+    It was about {number} {measure_of_time} ago when I came to 
+    the hospital in a {transportation_mode}. The hospital is a 
+    {descriptive_word} place, there are a lot of {descriptive_word2} 
+    {object1} here. There are nurses here who have {color} {part_of_body}. 
+    If someone wants to come into my room I told them that they have to 
+    {verb} first.I have decorated my room with {number2} {object2}. 
+    Today a doctor came into my room and they were wearing a 
+    {object3} on their {part_of_body2}. I heard that all doctors 
+    {verb2} {object4} everyday for breakfast. The most {descriptive_word3} 
+    thing about being in the hospital is the {silly_word} {object5}.
+    '''
 
 def madlib_3():
     """ Asks the user for input, validates the input and returns the string"""
